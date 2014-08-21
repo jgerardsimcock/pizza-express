7b747c86a8f82a9b566e1181c262e3d99d0b88de
jgerardsimcock <justin_simcock@yahoo.com>
2014-08-20 21:00:00 -1000

    changes made

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@ $.ajax('/orders', {
     method: "GET",
     success: function(data){
       console.log(data);
+      console.log(data[0].toppings);
+      console.log(data[0].sizes);
+
     },
     failure: function(error){
       console.log(error);
--- routes/orders.js
+++ routes/orders.js
@@ -5,12 +5,12 @@ var database = require('./../models/orders');
 // Orders Controller
 router.route('/')
   .get(function (req, res) {
-    // res.send("Fucking orders");
+    // res.send('omethin');
     console.log('orders route');
     database.getAllPizzaOrders(req, res);
   })
   .post(function (req, res) {
-    res.send("Fucking posting orders");
+    // res.send('something');
   })
   .delete(function (req, res) {
     res.send("Fucking deleting orders");
